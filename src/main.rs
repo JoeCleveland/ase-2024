4f85e7050ebbc520f674cef506f1e121541b3d01
JoeCleveland <33639708+JoeCleveland@users.noreply.github.com>
2024-01-30 13:54:24 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,31 @@ fn show_info() {
     eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
    show_info();
 
     // Parse command line arguments
     // First argument is input .wav file, second argument is output text file.
     let args: Vec<String> = std::env::args().collect();
-    // TODO: your code here
+    let in_file = &args[1];
+    let out_file = &args[2];
+    dbg!(&in_file);
 
     // Open the input wave file and determine number of channels
-    // TODO: your code here; see `hound::WavReader::open`.
+    let mut wav_file = hound::WavReader::open(in_file).unwrap();
+    let mut txt_file = File::create(out_file).unwrap();
+    let spec = wav_file.spec();
+    dbg!((&wav_file).len());
+    let mut channel = 0;
+    for sample in wav_file.samples::<i16>() {
+        let fval = sample.unwrap() as f32;
+        if channel == spec.channels {
+            writeln!(&mut txt_file)?;
+            channel = 0;
+        }
+        write!(&mut txt_file, "{} ", fval / 65535.0)?;
+        channel += 1;
+    }
 
-    // Read audio data and write it to the output text file (one column per channel)
-    // TODO: your code here; we suggest using `hound::WavReader::samples`, `File::create`, and `write!`.
-    //       Remember to convert the samples to floating point values and respect the number of channels!
+    Ok(())
 }
