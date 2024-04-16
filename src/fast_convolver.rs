14d625068a9b9fee872e8eff5956c5384d3a30b7
Joseph Cleveland <josephcleveland@Josephs-MacBook-Pro.local>
2024-04-16 12:39:46 -0400

    Start exc. 3

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -1,5 +1,7 @@
 struct FastConvolver {
-    // TODO: your fields here
+    mode: ConvolutionMode,
+    impulse_response: &[f32],
+    previous_block: Vec<f32>
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -10,7 +12,11 @@ pub enum ConvolutionMode {
 
 impl FastConvolver {
     pub fn new(impulse_response: &[f32], mode: ConvolutionMode) -> Self {
-        todo!("implement")
+        FastConvolver {
+            mode,
+            impulse_response,
+            previous_block: vec![0f32; impulse_response.len()]
+        }
     }
 
     pub fn reset(&mut self) {
@@ -18,7 +24,22 @@ impl FastConvolver {
     }
 
     pub fn process(&mut self, input: &[f32], output: &mut [f32]) {
-        todo!("implement")
+        if matches!(self.mode, ConvolutionMode::TimeDomain) {
+            let iL = self.impulse_response.len();
+            let pbL = self.previous_block.len();
+            for i in 0..input.len() {
+                let mut accum = 0.0;
+                for j in 0..self.impulse_response.len() {
+                    if i + j < iL - 1 {
+                        accum += self.impulse_response[j] * self.previous_block[pbL - (iL - 2) + i + j];
+                    } else {
+                        accum += self.impulse_response[j] * input[i + j - iL + 1];
+                    }
+                }
+                output[i] = accum;
+            }
+            self.previous_block = output;
+        }
     }
 
     pub fn flush(&mut self, output: &mut [f32]) {
