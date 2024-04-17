952f29c5c2d7a7ac84d505aeed7cd5b4a730729c
Joseph Cleveland <josephcleveland@Josephs-MacBook-Pro.local>
2024-04-16 21:39:44 -0400

    Impl flush function

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -1,6 +1,6 @@
 struct FastConvolver {
     mode: ConvolutionMode,
-    impulse_response: &[f32],
+    impulse_response: Vec<f32>,
     previous_block: Vec<f32>
 }
 
@@ -14,7 +14,7 @@ impl FastConvolver {
     pub fn new(impulse_response: &[f32], mode: ConvolutionMode) -> Self {
         FastConvolver {
             mode,
-            impulse_response,
+            impulse_response: impulse_response.to_vec(),
             previous_block: vec![0f32; impulse_response.len()]
         }
     }
@@ -27,7 +27,7 @@ impl FastConvolver {
         if matches!(self.mode, ConvolutionMode::TimeDomain) {
             let iL = self.impulse_response.len();
             let pbL = self.previous_block.len();
-            for i in 0..input.len() {
+            for i in 0..input.len() - iL {
                 let mut accum = 0.0;
                 for j in 0..self.impulse_response.len() {
                     if i + j < iL - 1 {
@@ -38,15 +38,24 @@ impl FastConvolver {
                 }
                 output[i] = accum;
             }
-            self.previous_block = output;
+            self.previous_block = output.to_vec();
         }
     }
 
     pub fn flush(&mut self, output: &mut [f32]) {
-        todo!("implement")
+        let iL = self.impulse_response.len();
+        for i in 0..self.previous_block.len() - iL {
+            let mut accum = 0.0;
+            for j in 0..self.impulse_response.len() {
+                accum += self.previous_block[i + j] * self.impulse_response[j];
+            }
+            output[i] = accum;
+        }
     }
 
-    // TODO: feel free to define other functions for your own use
+    pub fn get_flush_length(&mut self) -> usize {
+        return self.previous_block.len() - (self.impulse_response.len() - 1);
+    }
+
 }
 
-// TODO: feel free to define other types (here or in other modules) for your own use
